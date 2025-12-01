db64d86c0fd42d2d0399971dad6b7b91a4c620ec
Brandon <brandonnilly@gmail.com>
2025-11-30 18:02:27 -0700

    Initial Commit

---
+++ ChadBot/Modules/BirthdayModule.cs
@@ -0,0 +1,13 @@
+﻿using NetCord.Services.Commands;
+
+namespace ChadBot.Modules
+{
+    public class BirthdayModule : CommandModule<CommandContext>
+    {
+        [Command("list")]
+        public async Task<string> ListAsync()
+        {
+            return "This is a test for birthdays";
+        }
+    }
+}
---
+++ ChadBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddDiscordGateway();
+
+var host = builder.Build();
+
+host.AddModules(typeof(Program).Assembly);
+
+await host.RunAsync();
